ae41419730eccc6e406d0ce125cf33e23bab6d77
bearholmes <bearholmes@gmail.com>
2017-01-21 23:41:32 +0900

    1차 코드 최적화

--- js/background.js
+++ js/background.js
@@ -1,11 +1,10 @@
-
-var dkOverlayLoaded = false;
+/*jshint browser: true */
+/*global chrome */
 
 // Check whether new version is installed
 chrome.runtime.onInstalled.addListener(function(details) {
     if (details.reason == "install") {
         chrome.tabs.create( {url: "option.html"} );
-        //chrome.storage.local.set({'monitors': '17'});
     }
 });
 // || details.reason == "update" 제외
@@ -15,10 +14,6 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         alert("크롬 내부 페이지에서는 작동하지 않습니다."); //It does not work on Google Chrome Internal pages
         return;
     }
-
     chrome.tabs.executeScript(tab.id, {file: 'js/dkoverlay.js'});
-    //chrome.tabs.executeScript(tab.id, {file: 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js'});
     chrome.tabs.insertCSS(tab.id, {file: 'css/dkoverlay.css'});
-
-    dkOverlayLoaded = true;
 });
--- js/option.js
+++ js/option.js
@@ -1,40 +1,11 @@
 /*jshint browser: true */
 /*global chrome */
-
-var $ = function() {
-    return document.getElementById(arguments[0]);
-};
-
-function loadEvent() {
-    chrome.storage.local.get('monitors', function(result) {
-        var monitors = result.monitors;
-        if (monitors) {
-            $(monitors).selected = true;
-        }
-    });
-    chrome.storage.local.get('resolutions', function(result) {
-        var resolutions = result.resolutions;
-        if (resolutions) {
-            $(resolutions).selected = true;
-        }
-    });
+var manifest = chrome.runtime.getManifest();
+var name = manifest.name;
+var ver = manifest.version;
+window.onload = function()
+{
+document.title = name + "- Option";
+document.getElementById("dk_title").innerText = name;
+document.getElementById("dk_ver").innerText = "버전 : " + ver;
 }
-
-function resRegEvent() {
-    monitor = $("moniStd").value;
-    resolution = $("resStd").value;
-
-    chrome.storage.local.set({
-        'monitors': monitor
-    });
-    chrome.storage.local.set({
-        'resolutions': resolution
-    });
-
-    $("resStatus").innerText = "저장완료!";
-}
-
-window.addEventListener('DOMContentLoaded', function() {
-    $("resBtn").addEventListener('click', resRegEvent, false);
-    loadEvent();
-});
