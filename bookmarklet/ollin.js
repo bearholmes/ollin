e4a02b6e39ee084d976740b670b49a9dba09bc21
bearholmes <bearholmes@gmail.com>
2017-01-23 17:02:51 +0900

    Update ollin.js

--- bookmarklet/ollin.js
+++ bookmarklet/ollin.js
@@ -35,7 +35,7 @@
                         canvas.width = img.naturalWidth  || img.width;
                         canvas.height = img.naturalHeight || img.height;
                     };
-                    FR.readAsDataURL(fr.files[0]);
+                    fr.readAsDataURL(fr.files[0]);
                 }
 
                 let btn_elem = doc.getElementById("dk_overlay_btn");
