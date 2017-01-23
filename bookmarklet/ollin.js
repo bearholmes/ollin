98429f38d4a3f91ba9ae47ec31c53fa7fc14e614
bearholmes <bearholmes@gmail.com>
2017-01-23 17:27:56 +0900

    Update ollin.js

--- bookmarklet/ollin.js
+++ bookmarklet/ollin.js
@@ -73,7 +73,7 @@
         markup: {
             css: function() {
               var link = document.createElement("link");
-              link.href = "https://rawgit.com/bearholmes/ollin/master/bookmarkvar/ollin.css";
+              link.href = "https://rawgit.com/bearholmes/ollin/master/bookmarklet/ollin.css";
               link.type = "text/css";
               link.rel = "stylesheet";
               link.media = "all";
